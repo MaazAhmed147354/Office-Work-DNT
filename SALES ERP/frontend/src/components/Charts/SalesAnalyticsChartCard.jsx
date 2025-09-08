f69aa1c2989da891fb2bc576300d75ed999ae799
MaazAhmed147354 <maza147354@gmail.com>
2025-09-08 17:16:27 +0500

    Update styling of SalesAnalyticsChart

--- SALES ERP/frontend/src/components/Charts/SalesAnalyticsChartCard.jsx
+++ SALES ERP/frontend/src/components/Charts/SalesAnalyticsChartCard.jsx
@@ -9,7 +9,7 @@ const SalesAnalyticsChartCard = ({ tab, value, data }) => {
   //     );
   //   }
   console.log("Chart Data: ", data);
-  
+
   const labels = data.map((item) => item.label);
   const sales = data.map((item) => item.totalSales);
   console.log("Sales Tab: ", tab);
@@ -23,9 +23,10 @@ const SalesAnalyticsChartCard = ({ tab, value, data }) => {
         backgroundColor: [
           "rgb(173, 70, 255, 0.25)",
           "rgb(0, 117, 149, 0.25)",
-          "rgb(112, 8, 231, 0.5)",
+          "rgb(112, 8, 231, 0.25)",
+          "rgb(0, 132, 209, 0.25)",
         ],
-        borderColor: ["#c27aff", "#00b8db", "#8e51ff"],
+        borderColor: ["#c27aff", "#00b8db", "#8e51ff", "#0084d1"],
         borderWidth: 2,
         borderRadius: 5,
       },

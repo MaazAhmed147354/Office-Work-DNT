2d9accd85a6dacdad5e8f7c74a1215f1169eabc9
MaazAhmed147354 <maza147354@gmail.com>
2025-09-08 15:44:51 +0500

    Update components imports

--- SALES ERP/frontend/src/components/index.js
+++ SALES ERP/frontend/src/components/index.js
@@ -5,9 +5,9 @@ export { default as LoginForm } from "./Forms/LoginForm";
 
 export { default as StatCard } from "./Cards/StatCard";
 export { default as SalesTargetCard } from "./Cards/SalesTargetCard";
-export { default as DateTabCard } from "./Cards/DateTabCard";
 export { default as LeaderboardCard } from "./Cards/LeaderboardCard";
 export { default as WarrantyCard } from "./Cards/WarrantyCard";
+export { default as FilterDataCard } from "./Cards/FilterDataCard";
 
 export { default as GlobalPaginationTable } from "./Tables/GlobalPaginationTable";
 export { default as GlobalMarqueeTable } from "./Tables/GlobalMarqueeTable";
@@ -16,7 +16,7 @@ export { default as ProductsTable } from "./Tables/ProductsTable";
 export { default as TopProductsTableCard } from "./Tables/TopProductsTableCard";
 
 export { default as GlobalChart } from "./Charts/GlobalChart";
-export { default as PolarAreaChartCard } from "./Charts/PolarAreaChartCard";
+export { default as SalesContributionChartCard } from "./Charts/SalesContributionChartCard";
 export { default as RadarChartCard } from "./Charts/RadarChartCard";
 export { default as ProgressChartCard } from "./Charts/ProgressChartCard";
 export { default as SalesPersonChartCard } from "./Charts/SalesPersonChartCard";
@@ -24,11 +24,17 @@ export { default as ProductSalesChartCard } from "./Charts/ProductSalesChartCard
 export { default as MonthlySalesChartCard } from "./Charts/MonthlySalesChartCard";
 export { default as SalesAnalyticsChartCard } from "./Charts/SalesAnalyticsChartCard";
 
+export { default as DateTabs } from "./Tabs/DateTabs";
+export { default as AnalyticsTabs } from "./Tabs/AnalyticsTabs";
+
 export { default as DateModal } from "./Modals/DateModal";
 export { default as LogoutModal } from "./Modals/LogoutModal";
+export { default as SalesPersonModal } from "./Modals/SalesPersonModal";
 
 export { default as PrivateRoute } from "./Routes/PrivateRoute";
 
 export { default as SpinnerLoader } from "./Loaders/SpinnerLoader";
 
+export { default as CustomToast } from "./OtherComponents/CustomToast";
 export { default as Accordion } from "./OtherComponents/Accordion";
+export { default as CustomDropdown } from "./OtherComponents/CustomDropdown";

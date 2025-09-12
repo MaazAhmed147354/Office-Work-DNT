16cc014cc8ff5caea0145ad1906b26f9a0b506cc
MaazAhmed147354 <maza147354@gmail.com>
2025-09-12 11:47:51 +0500

    Adjust Responsiveness of FilterCard

--- SALES ERP/frontend/src/components/Cards/FilterDataCard.jsx
+++ SALES ERP/frontend/src/components/Cards/FilterDataCard.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { SlidersHorizontal } from "lucide-react";
+import { SlidersHorizontal, Funnel } from "lucide-react";
 import DateTabs from "../Tabs/DateTabs";
 import SalesPersonModal from "../Modals/SalesPersonModal";
 
@@ -16,23 +16,37 @@ const FilterDataCard = ({
 
   return (
     <div>
-      <div className="bg-[#252538] px-4 rounded-xl border border-[#38384a]">
-        <div className="flex flex-col md:flex-row md:items-center justify-between gap-4">
-          <div className="flex-1 ml-22">
-            <DateTabs
-              selectedTab={selectedTab}
-              setSelectedTab={setSelectedTab}
-              selectedValue={selectedValue}
-              setSelectedValue={setSelectedValue}
-            />
+      <div className="bg-[#252538] p-4 rounded-xl border border-[#38384a]">
+        <div className="flex flex-col lg:flex-row items-center justify-center gap-4 lg:gap-8">
+          {/* Date Selection Section */}
+          <div className="flex gap-2">
+            <h2 className="flex items-center gap-2 text-base md:text-md font-semibold text-[#BEB7DF] tracking-wide">
+              <Funnel />
+              <span className="hidden md:inline">Select Date</span>
+            </h2>
+            <div className="">
+              <DateTabs
+                selectedTab={selectedTab}
+                setSelectedTab={setSelectedTab}
+                selectedValue={selectedValue}
+                setSelectedValue={setSelectedValue}
+              />
+            </div>
           </div>
-          <div>
-            <button
-              onClick={() => setIsSalesModalOpen(true)}
-              className="px-4 py-2 rounded-lg bg-[#38384a] text-gray-300 hover:bg-[#8a4fff] hover:text-white transition-colors whitespace-nowrap"
-            >
-              <SlidersHorizontal />
-            </button>
+          {/* Salesperson Selection Section */}
+          <div className="flex gap-2">
+            <h2 className="flex items-center gap-2 text-base md:text-md font-semibold text-[#BEB7DF] tracking-wide">
+              <Funnel />
+              <span className="hidden md:inline">Select Salespersons</span>
+            </h2>
+            <div>
+              <button
+                onClick={() => setIsSalesModalOpen(true)}
+                className="px-4 py-2 gap-2 rounded-lg bg-[#38384a] text-gray-300 hover:bg-[#8a4fff] hover:text-white transition-colors whitespace-nowrap"
+              >
+                <SlidersHorizontal />
+              </button>
+            </div>
           </div>
         </div>
       </div>
